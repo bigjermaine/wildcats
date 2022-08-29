7695069479a506f306ab87d27a1450747b07df36
daniel jermaine <danieljermaine2@gmail.com>
2022-08-29 09:33:45 +0100

    Initial Commit

---
+++ ShoppingApplication/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShoppingApplication
+//
+//  Created by Apple on 29/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShoppingApplication/ShoppingApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingApplicationApp.swift
+//  ShoppingApplication
+//
+//  Created by Apple on 29/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
