c9dd8062ec732f2cdd908aa849c55f4f7ec23d16
daniel jermaine <danieljermaine2@gmail.com>
2022-09-02 19:01:20 +0100

    Initial Commit

---
+++ WildCats/WildCatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WildCatsApp.swift
+//  WildCats
+//
+//  Created by Apple on 02/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct WildCatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
